722c751a76ac6e43d00eb638b13086c703b0a32d
chauwei150 <clee6846@yahoo.com>
2022-12-30 17:28:14 +0000

    Second push

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask! Deployed using ArgoCD'
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
