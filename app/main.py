a281b62317b6d95a7708fd831faed19a1df8e7e0
chauwei150 <clee6846@yahoo.com>
2022-12-30 18:52:54 +0000

    modify main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates') )
 
 @app.route('/')
-def main():
+def form():
     return render_template('form.html')
 
 if __name__ == "__main__":
