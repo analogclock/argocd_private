0fc41c470d556801f275b74637ac16cab8749fc4
chauwei150 <clee6846@yahoo.com>
2022-12-30 18:38:34 +0000

    version 2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__)
 
 @app.route('/')
-def index():
+def main():
     return render_template('form.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port= '5000')
